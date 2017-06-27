5eca605392ce18c9d7b8c44537e75989eef4f3ba
utkarsh <utkarshrai003@gmail.com>
2017-06-28 00:48:48 +0530

    Adds methods to create an appointment

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -13,4 +13,24 @@ class Appointment < ApplicationRecord
   # Validations
   validates_presence_of :patient, :physician
 
+  # Class Methods
+  # Method to create an appointment between physician and patient
+  def self.make_between(physician, patient, diseases)
+    appointment = Appointment.new(physician: physician, patient: patient, diseases: diseases)
+    unless Appointment.pending?(physician, patient)
+      appointment.save
+    else
+      raise "Appointment already made"
+    end
+  end
+
+  # Method to check if a duplicate pending appointment exists
+  def self.pending?(physician, patient)
+    Appointment.where(
+      physician: physician,
+      patient: patient,
+      pending: true
+    ).present?
+  end
+
 end
