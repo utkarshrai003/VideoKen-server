37aa541b3739ce6b1571efeb64f1507a368b7997
utkarsh <utkarshrai003@gmail.com>
2017-06-28 01:26:11 +0530

    Changed rename column to change column in migration

--- db/migrate/20170627194833_update_specialization_column_to_string_in_users.rb
+++ db/migrate/20170627194833_update_specialization_column_to_string_in_users.rb
@@ -1,5 +1,5 @@
 class UpdateSpecializationColumnToStringInUsers < ActiveRecord::Migration[5.1]
   def change
-    rename_column :users, :specialization, :string 
+    change_column :users, :specialization, :string 
   end
 end
