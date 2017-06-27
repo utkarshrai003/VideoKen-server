e2f654df69d52f57a29659111471c4bbb785304f
utkarsh <utkarshrai003@gmail.com>
2017-06-27 18:18:47 +0530

    Adds Validation for User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,19 @@
 class User < ApplicationRecord
+
+  # Enumeration for specialization
+  # Only for Physician Role
+  enum specialization: [
+    "Physician",
+    "Therapist",
+    "Nursing Assistant",
+    "Pharmacists"
+  ]
+
+  # Validations
+  validates_presence_of :name, :email
+
+  # Associations
+
+  # Callbacks
+
 end
