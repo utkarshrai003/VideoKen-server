aed04548862cc84b20c910146a529da648845bed
utkarsh <utkarshrai003@gmail.com>
2017-06-27 20:03:50 +0530

    Adds Api namespacing to Routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,14 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # Api Routes
+  api_routes = lambda do
+    resources :users, only: [:create]
+  end
+
+  # Route Namespacing
+  namespace :api do
+    namespace :v1, &api_routes
+  end
+
 end
