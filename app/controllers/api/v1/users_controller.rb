5ea0f810b68008cfac7a9cb0ab06d8c94b95867d
utkarsh <utkarshrai003@gmail.com>
2017-06-27 22:03:53 +0530

    Adds endpoint to create a user with specified role

--- app/controllers/api/v1/users_controller.rb
+++ app/controllers/api/v1/users_controller.rb
@@ -1,16 +1,43 @@
-class UsersController < ApplicationController
+class Api::V1::UsersController < ApplicationController
+
+  before_action :set_role, only: [:create]
 
   # Endpoint to create patient and physician users
   def create
-    
+    user = User.create(@user_info)
+    if user
+      render json: { status: 200, data: UserSerializer.new(user).as_json }
+    else
+      render json: { status: 400, error: user.errors.full_messages }
+    end
   end
 
   private
 
   # Never trust the parameters from the Internet
   def user_params
-    params.require(:user)
-    .permit(:name, :number, :age, :email, :specialization)
+    params.require(:user).permit(
+      :name,
+      :number,
+      :age,
+      :email,
+      :specialization,
+      :role
+    )
+  end
+
+  # Method to load the role object in the User params
+  def user_params_with_role(role)
+    return unless role
+    @user_info = user_params
+    @user_info.delete(:role)
+    @user_info[:role] = role
+  end
+
+  # Method to verify and set the role for the user
+  def set_role
+    role = Role.find_by_name(user_params[:role]) rescue nil
+    role ? user_params_with_role(role) : (render json: { status: 400, error: "No such role" })
   end
 
 end
