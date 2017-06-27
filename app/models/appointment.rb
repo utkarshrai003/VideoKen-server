699dda6057f3501c9ecef9de77b38489445acc60
utkarsh <utkarshrai003@gmail.com>
2017-06-27 23:25:20 +0530

    Adds Associations and Validations to Appointments model

--- app/models/appointment.rb
+++ app/models/appointment.rb
@@ -1,2 +1,16 @@
 class Appointment < ApplicationRecord
+
+  # Associations
+  belongs_to :patient, {
+    class_name: 'User',
+    foreign_key: :patient_id
+  }
+  belongs_to :physician, {
+    class_name: 'User',
+    foreign_key: :physician_id
+  }
+
+  # Validations
+  validates_presence_of :patient, :physician
+
 end
