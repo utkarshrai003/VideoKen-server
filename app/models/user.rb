c9c19fa48884f9776a50de780070d7971874ded6
utkarsh <utkarshrai003@gmail.com>
2017-06-27 23:35:54 +0530

    Adds associations for Appointments in User model

--- app/models/user.rb
+++ app/models/user.rb
@@ -15,11 +15,19 @@ class User < ApplicationRecord
     dependent: :destroy
   }
   has_one :role, through: :user_role
+  has_many :received_appointments, {
+    foreign_key: :physician_id,
+    class_name: 'Appointment',
+    dependent: :destroy
+    }
+  has_many :booked_appointments, {
+    foreign_key: :patient_id,
+    class_name: 'Appointment',
+    dependent: :destroy
+  }
 
   # Validations
   validates_presence_of :name, :email
-
-  # Associations
 
   # Callbacks
 
