e5efb6f81adfc5d59949c0f84f17e97c742e16fa
utkarsh <utkarshrai003@gmail.com>
2017-06-27 18:27:44 +0530

    Adds Validation for Role Model

--- app/models/role.rb
+++ app/models/role.rb
@@ -1,2 +1,15 @@
 class Role < ApplicationRecord
+
+  # Validations
+  validates :name, :presence: true, uniqueness: true
+
+  # Instance Methods
+  def underscored_name
+    name.to_s.downcase.gsub(' ', '_')
+  end
+
+  def pluralized_name
+    underscored_name.pluralize
+  end
+  
 end
