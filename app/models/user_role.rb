bc3d5644d03993027043b2fd10d3de631d2dccbe
utkarsh <utkarshrai003@gmail.com>
2017-06-27 18:33:26 +0530

    Adds Validation, Association and helper methods to User Role Model

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,2 +1,28 @@
 class UserRole < ApplicationRecord
+
+  # Associations
+  belongs_to :user, :inverse_of => :user_role
+  belongs_to :role, :inverse_of => :user_role
+
+  # Validations
+  validates :user, :presence => true
+  validates :role, :presence => true
+  validates_uniqueness_of :user_id, :scope => :role_id
+
+  begin
+    Role.all.each do | role |
+      # Scopes
+      # Provides scope lik. physicians, patients
+      scope role.pluralized_name, -> { where(:role => role) }
+
+      # Helper method
+      # Provides helpers like. admin?, patient?, dispensary?
+      define_method "#{role.underscored_name}?" do
+        self.role.try(:name).eql?(role.name)
+      end
+    end
+  rescue StandardError => e
+    Rails.logger.info "Unable to load helpers"
+  end
+
 end
