72e0b848fc06a71c2907f0c81e8c709c1897f514
utkarsh <utkarshrai003@gmail.com>
2017-06-30 02:04:14 +0530

    Adds routes for patient and physician endpoints

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,9 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
+  get '/api/v1/patients' => 'api/v1/users#patients'
+  get '/api/v1/physicians' => 'api/v1/users#physicians'
+
   # Api Routes
   api_routes = lambda do
     resources :users, only: [:create]
