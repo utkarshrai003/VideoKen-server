448830b97711bd165463e872dd0ae0ffc84ea80e
utkarsh <utkarshrai003@gmail.com>
2017-06-30 02:03:37 +0530

    Adds class methods to return users with patient and physician roles

--- app/models/user.rb
+++ app/models/user.rb
@@ -21,4 +21,15 @@ class User < ApplicationRecord
   validates_presence_of :name, :email, :role
   validates_uniqueness_of :email
 
+  # Class Methods
+  def self.patients
+    patient_roles = UserRole.includes(:user).all.where(role: Role.find_by_name('patient'))
+    patient_roles.map {|role| {id: role.user.id, name: role.user.name}}
+  end
+
+  def self.physicians
+    physician_roles = UserRole.includes(:user).all.where(role: Role.find_by_name('physician'))
+    physician_roles.map {|role| {id: role.user.id, name: role.user.name}}
+  end
+
 end
